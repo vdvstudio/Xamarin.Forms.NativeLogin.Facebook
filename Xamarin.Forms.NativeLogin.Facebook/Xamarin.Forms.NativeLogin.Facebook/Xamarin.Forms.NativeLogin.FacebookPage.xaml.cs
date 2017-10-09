aa783e92b5de96ec770bef64218bee12a8ef64de
Lachlan van der Velden <lvelden1@gmail.com>
2017-10-09 16:25:18 +1100

    Initial Project Creation

---
+++ Xamarin.Forms.NativeLogin.Facebook/Xamarin.Forms.NativeLogin.Facebook/Xamarin.Forms.NativeLogin.FacebookPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin.Forms.NativeLogin.Facebook
+{
+    public partial class Xamarin_Forms_NativeLogin_FacebookPage : ContentPage
+    {
+        public Xamarin_Forms_NativeLogin_FacebookPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
